894b54b582790a5051825636d33f2a13183b23f7
Alexander Klenin <klenin@gmail.com>
2019-08-04 23:09:41 +1000

    Clone: Implement deep clone. See #74

--- Yuzu/Clone.cs
+++ Yuzu/Clone.cs
@@ -1,21 +1,225 @@
-﻿using Yuzu.Metadata;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Yuzu.Metadata;
+using Yuzu.Util;
 
 namespace Yuzu.Clone
 {
 	public class Cloner
 	{
 		public CommonOptions Options;
+		public static Cloner Instance = new Cloner();
 
-		public T ShallowClone <T>(T obj) => (T)ShallowCloneObject(obj);
+		public T Shallow <T>(T src) => (T)ShallowObject(src);
 
-		public object ShallowCloneObject(object obj)
+		public object ShallowObject(object src)
 		{
-			var meta = Meta.Get(obj.GetType(), Options);
+			var meta = Meta.Get(src.GetType(), Options);
 			var result = meta.Factory();
-			foreach (var item in meta.Items) {
-				item.SetValue(result, item.GetValue(obj));
-			}
+			foreach (var item in meta.Items)
+				item.SetValue(result, item.GetValue(src));
 			return result;
 		}
+
+		public T Deep<T>(T obj) => (T)DeepObject(obj);
+
+		private Dictionary<Type, Func<object, object>> clonerCache =
+			new Dictionary<Type, Func<object, object>>();
+
+		private bool IsCopyable(Type t) =>
+			t.IsPrimitive || t.IsValueType || t == typeof(string);
+
+		private T[] CloneArray<T>(object src, Func<object, object> cloneElem)
+		{
+			var a = (T[])src;
+			var result = new T[a.Length];
+			for (int i = 0; i < a.Length; ++i)
+				result[i] = (T)cloneElem(a[i]);
+			return result;
+		}
+
+		private T[] CloneArrayPrimitive<T>(object src)
+		{
+			var a = (T[])src;
+			var result = new T[a.Length];
+			Array.Copy(a, result, a.Length);
+			return result;
+		}
+
+		protected I CloneIDictionary<I, K, V>(
+			object src, Func<object, object> cloneKey, Func<object, object> cloneValue
+		) where I : class, IDictionary<K, V>, new()
+		{
+			if (src == null)
+				return null;
+			var result = new I();
+			foreach (var kv in (I)src)
+				result.Add((K)cloneKey(kv.Key), (V)cloneValue(kv.Value));
+			return result;
+		}
+
+		private I CloneIDictionaryPrimiviteKey<I, K, V>(
+			object src, Func<object, object> cloneValue
+		) where I : class, IDictionary<K, V>, new()
+		{
+			if (src == null)
+				return null;
+			var result = new I();
+			foreach (var kv in (I)src)
+				result.Add(kv.Key, (V)cloneValue(kv.Value));
+			return result;
+		}
+
+		private I CloneIDictionaryPrimivite<I, K, V>(object src)
+			where I : class, IDictionary<K, V>, new()
+		{
+			if (src == null)
+				return null;
+			var result = new I();
+			foreach (var kv in (I)src)
+				result.Add(kv);
+			return result;
+		}
+
+		protected I CloneCollection<I, E>(object src, Func<object, object> cloneElem)
+			where I : class, ICollection<E>, new()
+		{
+			if (src == null)
+				return null;
+			var result = new I();
+			foreach (var item in (I)src)
+				result.Add((E)cloneElem(item));
+			return result;
+		}
+
+		protected I CloneCollectionPrimitive<I, E>(object src)
+			where I : class, ICollection<E>, new()
+		{
+			if (src == null)
+				return null;
+			var result = new I();
+			foreach (var item in (I)src)
+				result.Add(item);
+			return result;
+		}
+
+		private object ValueCopy(object src) => src;
+		private object CloneNull(object srcr) => null;
+
+		public Func<object, object> GetCloner(Type t)
+		{
+			Func<object, object> cloner;
+			if (!clonerCache.TryGetValue(t, out cloner)) {
+				cloner = MakeCloner(t);
+				clonerCache.Add(t, cloner);
+			}
+			return cloner;
+		}
+
+		protected Func<object, object> MakeDelegateFunc(MethodInfo m) =>
+			(Func<object, object>)Delegate.CreateDelegate(typeof(Func<object, object>), this, m);
+
+		protected Func<object, TParam, object> MakeDelegateParam<TParam>(MethodInfo m) =>
+			(Func<object, TParam, object>)Delegate.CreateDelegate(
+				typeof(Func<object, TParam, object>), this, m);
+
+		protected Func<object, TParam1, TParam2, object> MakeDelegateParam2<TParam1, TParam2>(MethodInfo m) =>
+			(Func<object, TParam1, TParam2, object>)Delegate.CreateDelegate(
+				typeof(Func<object, TParam1, TParam2, object>), this, m);
+
+		private Func<object, object> MakeCloner(Type t)
+		{
+			if (IsCopyable(t)) return ValueCopy;
+			if (t.IsGenericType) {
+				var g = t.GetGenericTypeDefinition();
+				if (g == typeof(Nullable<>)) {
+					var c = GetCloner(t.GetGenericArguments()[0]);
+					return src => src ?? c(src);
+				}
+			}
+			if (t.IsArray) {
+				var e = t.GetElementType();
+				if (IsCopyable(e)) {
+					var m = Utils.GetPrivateGeneric(GetType(), nameof(CloneArrayPrimitive), e);
+					return MakeDelegateFunc(m);
+				}
+				else {
+					var cloneElem = GetCloner(e);
+					var m = Utils.GetPrivateGeneric(GetType(), nameof(CloneArray), e);
+					var d = MakeDelegateParam<Func<object, object>>(m);
+					return src => d(src, cloneElem);
+				}
+			}
+			{
+				var idict = Utils.GetIDictionary(t);
+				if (idict != null) {
+					var a = idict.GetGenericArguments();
+					if (!IsCopyable(a[0])) {
+						var сk = GetCloner(a[0]);
+						var сv = GetCloner(a[1]);
+						var m = Utils.GetPrivateGeneric(
+							GetType(), nameof(CloneIDictionary), t, a[0], a[1]);
+						var d = MakeDelegateParam2<Func<object, object>, Func<object, object>>(m);
+						return obj => d(obj, сk, сv);
+					}
+					else if (!IsCopyable(a[1])) {
+						var сv = GetCloner(a[1]);
+						var m = Utils.GetPrivateGeneric(
+							GetType(), nameof(CloneIDictionaryPrimiviteKey), t, a[0], a[1]);
+						var d = MakeDelegateParam<Func<object, object>>(m);
+						return obj => d(obj, сv);
+					}
+					else {
+						var m = Utils.GetPrivateGeneric(
+							GetType(), nameof(CloneIDictionaryPrimivite), t, a[0], a[1]);
+						return MakeDelegateFunc(m);
+					}
+				}
+			}
+			{
+				var icoll = Utils.GetICollection(t);
+				if (icoll != null) {
+					var a = icoll.GetGenericArguments();
+					if (!IsCopyable(a[0])) {
+						var сe = GetCloner(a[0]);
+						var m = Utils.GetPrivateGeneric(GetType(), nameof(CloneCollection), t, a[0]);
+						var d = MakeDelegateParam<Func<object, object>>(m);
+						return obj => d(obj, сe);
+					}
+					else {
+						var m = Utils.GetPrivateGeneric(
+							GetType(), nameof(CloneCollectionPrimitive), t, a[0]);
+						return MakeDelegateFunc(m);
+					}
+				}
+			}
+			if (t.IsClass || t.IsInterface || Utils.IsStruct(t)) {
+				var meta = Meta.Get(t, Options);
+				if (meta.Items.Count == 0)
+					return CloneNull;
+				var cloners = new Func<object, object>[meta.Items.Count];
+				return src => {
+					if (cloners[0] == null) {
+						// Initialize 'cloners' lazily to prevent infinite recursion.
+						int i = 0;
+						foreach (var item in meta.Items)
+							cloners[i++] = GetCloner(item.Type);
+					}
+					if (src == null)
+						return null;
+					var result = meta.Factory();
+					int j = 0;
+					foreach (var item in meta.Items)
+						item.SetValue(result, cloners[j++](item.GetValue(src)));
+					return result;
+				};
+
+			}
+			throw new NotImplementedException("Unable to clone type: " + t.FullName);
+		}
+
+		public object DeepObject(object src) => GetCloner(src.GetType())(src);
 	}
 }
--- Yuzu/YuzuTest/TestClone.cs
+++ Yuzu/YuzuTest/TestClone.cs
@@ -16,25 +16,148 @@ namespace YuzuTest
 
 			{
 				var src = new Sample1 { X = 9, Y = "qwe" };
-				var dst = (Sample1)cl.ShallowCloneObject(src);
+				var dst = (Sample1)cl.ShallowObject(src);
 				Assert.AreEqual(src.X, dst.X);
 				Assert.AreEqual(src.Y, dst.Y);
 			}
 			{
 				var src = new Sample2 { X = 19, Y = "qwe" };
-				var dst = cl.ShallowClone(src);
+				var dst = cl.Shallow(src);
 				Assert.AreEqual(src.X, dst.X);
 				Assert.AreEqual(src.Y, dst.Y);
 			}
 			{
 				var src = new SampleDict {
-					Value = 1, Children = new Dictionary<string, SampleDict> {
+					Value = 1,
+					Children = new Dictionary<string, SampleDict> {
 						{ "a", new SampleDict { Value = 2 } }
 					}
 				};
-				var dst = cl.ShallowClone(src);
+				var dst = cl.Shallow(src);
 				Assert.AreEqual(src.Value, dst.Value);
 				Assert.AreEqual(src.Children, dst.Children);
+			}
+		}
+
+		[TestMethod]
+		public void TestBasic()
+		{
+			var cl = new Cloner();
+
+			{
+				var src = new Sample1 { X = 9, Y = "qwe" };
+				var dst = (Sample1)cl.DeepObject(src);
+				Assert.AreNotEqual(src, dst);
+				Assert.AreEqual(src.X, dst.X);
+				Assert.AreEqual(src.Y, dst.Y);
+			}
+			{
+				var src = new Sample2 { X = 19, Y = "qwe" };
+				var dst = cl.Deep(src);
+				Assert.AreNotEqual(src, dst);
+				Assert.AreEqual(src.X, dst.X);
+				Assert.AreEqual(src.Y, dst.Y);
+			}
+			{
+				var src = new Sample3 { S1 = new Sample1 { X = 19, Y = "qwe" } };
+				var dst = cl.Deep(src);
+				Assert.AreNotEqual(src, dst);
+				Assert.AreNotEqual(src.S1, dst.S1);
+				Assert.AreEqual(src.S1.X, dst.S1.X);
+				Assert.AreEqual(src.S1.Y, dst.S1.Y);
+			}
+		}
+
+		[TestMethod]
+		public void TestCollection()
+		{
+			var cl = new Cloner();
+
+			{
+				var src = new int[5] { 2, 4, 5, 6, 8 };
+				var dst = cl.Deep(src);
+				CollectionAssert.AreEqual(src, dst);
+			}
+			{
+				var src = new Sample1[1] { new Sample1 { X = 33 } };
+				var dst = cl.Deep(src);
+				Assert.AreNotEqual(src[0], dst[0]);
+				Assert.AreEqual(src[0].X, dst[0].X);
+			}
+			{
+				var src = new List<string> { "s1", "s2" };
+				var dst = cl.Deep(src);
+				Assert.AreNotEqual(src, dst);
+				CollectionAssert.AreEqual(src, dst);
+			}
+			{
+				var src = new List<Sample1> { new Sample1 { X = 34 } };
+				var dst = cl.Deep(src);
+				Assert.AreNotEqual(src, dst);
+				Assert.AreEqual(src.Count, dst.Count);
+				Assert.AreNotEqual(src[0], dst[0]);
+				Assert.AreEqual(src[0].X, dst[0].X);
+			}
+			{
+				var src = new SampleCollection<int> { 1, 5 };
+				var dst = cl.Deep(src);
+				Assert.AreNotEqual(src, dst);
+				int[] srcA = new int[2], dstA = new int[2];
+				src.CopyTo(srcA, 0);
+				dst.CopyTo(dstA, 0);
+				CollectionAssert.AreEqual(srcA, dstA);
+			}
+		}
+
+		[TestMethod]
+		public void TestDict()
+		{
+			var cl = new Cloner();
+			{
+				var src = new Dictionary<string, int> { { "a", 1 }, { "b", 2 } };
+				var dst = cl.Deep(src);
+				Assert.AreNotEqual(src, dst);
+				Assert.AreEqual(src.Count, dst.Count);
+				Assert.AreEqual(src["a"], dst["a"]);
+				Assert.AreEqual(src["b"], dst["b"]);
+			}
+			{
+				var src = new SampleDict {
+					Value = 1,
+					Children = new Dictionary<string, SampleDict> {
+						{ "a", new SampleDict { Value = 2 } }
+					}
+				};
+				var dst = cl.Deep(src);
+				Assert.AreEqual(src.Value, dst.Value);
+				Assert.AreNotEqual(src.Children, dst.Children);
+				Assert.AreEqual(src.Children["a"].Value, dst.Children["a"].Value);
+			}
+			{
+				var src = new SampleDictKeys {
+					E = new Dictionary<SampleEnum, int> { { SampleEnum.E2, 6 } },
+					I = null,
+					K = new Dictionary<SampleKey, int> { { new SampleKey { V = 7 }, 8 } }
+				};
+				var dst = cl.Deep(src);
+				Assert.AreNotEqual(src.E, dst.E);
+				Assert.AreNotEqual(src.K, dst.K);
+				Assert.AreEqual(src.I, dst.I);
+				Assert.AreEqual(src.E[SampleEnum.E2], dst.E[SampleEnum.E2]);
+				Assert.AreEqual(src.K[new SampleKey { V = 7 }], dst.K[new SampleKey { V = 7 }]);
+			}
+		}
+
+		[TestMethod]
+		public void TestNullable()
+		{
+			var cl = new Cloner();
+
+			{
+				var src = new List<int?> { 3, null, 5 };
+				var dst = cl.Deep(src);
+				Assert.AreNotEqual(src, dst);
+				CollectionAssert.AreEqual(src, dst);
 			}
 		}
 	}
