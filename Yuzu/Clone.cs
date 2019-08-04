da749925e8b09691bbd2da44bb048140645f838e
Alexander Klenin <klenin@gmail.com>
2019-08-04 20:32:35 +1000

    Clone: Initial implementation. See #74

---
+++ Yuzu/Clone.cs
@@ -0,0 +1,21 @@
+﻿using Yuzu.Metadata;
+
+namespace Yuzu.Clone
+{
+	public class Cloner
+	{
+		public CommonOptions Options;
+
+		public T ShallowClone <T>(T obj) => (T)ShallowCloneObject(obj);
+
+		public object ShallowCloneObject(object obj)
+		{
+			var meta = Meta.Get(obj.GetType(), Options);
+			var result = meta.Factory();
+			foreach (var item in meta.Items) {
+				item.SetValue(result, item.GetValue(obj));
+			}
+			return result;
+		}
+	}
+}
---
+++ Yuzu/YuzuTest/TestClone.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Yuzu.Clone;
+
+namespace YuzuTest
+{
+	[TestClass]
+	public class TestClone
+	{
+		[TestMethod]
+		public void TestShallow()
+		{
+			var cl = new Cloner();
+
+			{
+				var src = new Sample1 { X = 9, Y = "qwe" };
+				var dst = (Sample1)cl.ShallowCloneObject(src);
+				Assert.AreEqual(src.X, dst.X);
+				Assert.AreEqual(src.Y, dst.Y);
+			}
+			{
+				var src = new Sample2 { X = 19, Y = "qwe" };
+				var dst = cl.ShallowClone(src);
+				Assert.AreEqual(src.X, dst.X);
+				Assert.AreEqual(src.Y, dst.Y);
+			}
+			{
+				var src = new SampleDict {
+					Value = 1, Children = new Dictionary<string, SampleDict> {
+						{ "a", new SampleDict { Value = 2 } }
+					}
+				};
+				var dst = cl.ShallowClone(src);
+				Assert.AreEqual(src.Value, dst.Value);
+				Assert.AreEqual(src.Children, dst.Children);
+			}
+		}
+	}
+}
